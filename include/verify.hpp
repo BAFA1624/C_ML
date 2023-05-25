093ca06ad9d78ca768b93474f4d8fb2a28a01fed
Ben Andrews <benafandrews@gmail.com>
2023-05-25 03:45:31 +0100

    Fixed linking issue.

--- include/verify.hpp
+++ include/verify.hpp
@@ -4,8 +4,25 @@
 #include <concepts>
 #include <vector>
 
-template <std::floating_point T, std::size_t N>
-constexpr inline T MSE( const std::array<T, N> & predictions,
-                        const std::array<T, N> & labels );
 template <std::floating_point T>
-T MSE( const std::vector<T> & predictions, const std::vector<T> & labels );
+T
+MSE( const std::vector<T> & predictions, const std::vector<T> & labels ) {
+    assert( predictions.size() == labels.size() );
+
+    T MSE{ 0. };
+    for ( std::uint64_t i{ 0 }; i < predictions.size(); ++i ) {
+        MSE += ( predictions[i] - labels[i] ) * ( predictions[i] - labels[i] );
+    }
+
+    return MSE;
+}
+
+template <std::floating_point T, std::size_t N>
+inline constexpr T
+MSE( const std::array<T, N> & predictions, const std::array<T, N> & labels ) {
+    T MSE{ 0. };
+    for ( std::uint64_t i{ 0 }; i < N; ++i ) {
+        MSE += ( predictions[i] - labels[i] ) * ( predictions[i] - labels[i] );
+    }
+    return MSE;
+}
