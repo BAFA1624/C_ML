35fd827108b9295b4760c639fc69a43516ae0089
Ben Andrews <benafandrews@gmail.com>
2023-05-25 04:16:31 +0100

    Begun working on Neural Network in CPP.

--- include/neuron.hpp
+++ include/neuron.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "activation_functions.hpp"
+
 #include <cmath>
 #include <concepts>
 #include <functional>
@@ -13,12 +15,23 @@ template <std::floating_point T>
 class Neuron
 {
     private:
-    T                        m_weight;
-    std::vector<Neuron<T> &> m_outputs;
-    activation_func<T>       m_activation;
+    T                       m_weight;
+    std::vector<Neuron<T> > m_outputs;
+    activation_func<T>      m_activation;
 
     public:
+    Neuron( const T initial_weight, const activation_func<T> f ) :
+        m_weight( initial_weight ), m_activation( f ) {}
+
+    virtual void add_outputs( const std::vector<Neuron<T> > & outputs ) {
+        m_outputs.insert( m_outputs.end(), outputs.begin(), outputs.end() );
+    };
 };
 
 template <std::floating_point T>
-using OutputNeuron = Neuron<T>;
+class OutputNeuron : public Neuron<T>
+{
+    public:
+    void
+    add_outputs( const std::vector<Neuron<T> > & outputs ) override = delete;
+};
