b7047933705da1a7f4f7ebd2d0c97ba3e31aa5ab
Ben Andrews <benafandrews@gmail.com>
2023-06-18 03:32:34 +0100

    Tested activation function (sigmoid) works as intended.

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,8 @@ main() {
     std::cout << "Hello, world!" << std::endl;
 
     neural::NeuralNetwork<double> test( 10, 4, { 5, 4, 8 },
+                                        { neural::sigmoid<double>,
+                                          neural::sigmoid<double>,
+                                          neural::sigmoid<double> },
                                         std::time( nullptr ) );
 }
