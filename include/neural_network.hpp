13ba63da8599cd7b4896cd1f628d0592b3b535e8
Ben Andrews <benafandrews@gmail.com>
2023-06-18 02:02:12 +0100

    Added is_complex struct to neural namespace.

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -4,6 +4,15 @@ namespace neural
 {
 
 template <typename T>
+struct is_complex : std::false_type
+{};
+template <std::floating_point T>
+struct is_complex<std::complex<T>> : std::true_type
+{};
+
+
+
+template <typename T>
 class NeuralNetwork
 {};
 
