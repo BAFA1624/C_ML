0769f517c09d2aadf33e7e34c4710b6818e78590
Ben Andrews <benafandrews@gmail.com>
2023-06-18 02:04:05 +0100

    Defined weight_type concept to allow any floating point or complex type.

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -13,7 +13,8 @@ template <std::floating_point T>
 struct is_complex<std::complex<T>> : std::true_type
 {};
 
-
+template <typename T>
+concept weight_type = std::floating_point<T> || is_complex<T>::value;
 
 template <typename T>
 class NeuralNetwork
