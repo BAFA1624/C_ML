ea7e5c41177a1a331e9db92527272f05abcc2750
Ben Andrews <benafandrews@gmail.com>
2023-09-21 00:13:33 +0100

    Altered main.cpp to test new additions.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,19 @@ int
 main() {
     std::cout << "Hello, world!" << std::endl;
 
-    neural::NeuralNetwork<double> test( 10, 4, { 5, 4, 8 },
-                                        { neural::sigmoid<double>,
-                                          neural::sigmoid<double>,
-                                          neural::sigmoid<double> },
-                                        std::time( nullptr ) );
+    neural::NeuralNetwork<double> test(
+        { 3, 4, 5, 6, 3 },
+        { neural::sigmoid<double>, neural::sigmoid<double>,
+          neural::sigmoid<double>, neural::sigmoid<double> },
+        std::time( nullptr ) );
+
+    const auto shape = test.shape();
+
+    std::cout << "Network shape:" << std::endl;
+    for ( const auto & x : shape ) { std::cout << x << std::endl; }
+
+    const auto res = test.forward(
+        std::vector<double>{ 0., 1., 2., 3., 4., 5., 6., 7., 8., 9. } );
+    std::cout << "Testing forwarding:" << std::endl;
+    for ( const auto & x : res ) { std::cout << x << std::endl; }
 }
