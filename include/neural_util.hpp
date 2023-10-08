0616d86298e4150a011930f606e11388f15aaeef
Ben Andrews <benafandrews@gmail.com>
2023-10-08 20:40:21 +0100

    Changed to Eigen in utils.

--- include/neural_util.hpp
+++ include/neural_util.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Eigen/Dense"
+
 #include <complex>
 #include <functional>
 #include <utility>
@@ -18,24 +20,21 @@ struct is_complex<std::complex<T>> : std::true_type
 template <typename T>
 concept Weight = std::floating_point<T> || is_complex<T>::value;
 
-// Pair of weight & bias
 template <Weight T>
-using neuron_weight_t = std::vector<T>;
-
-template <Weight T>
-using neuron_t = std::pair<neuron_weight_t<T>, T>;
-
-template <Weight T>
-using layer_t = std::vector<neuron_t<T>>;
+using layer_t = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
 template <Weight T>
 using network_t = std::vector<layer_t<T>>;
 
 template <Weight T>
-using output_layer_t = std::vector<T>;
+using output_layer_t = layer_t<T>;
 template <Weight T>
-using output_network_t = std::vector<output_layer_t<T>>;
+using output_network_t = std::vector<layer_t<T>>;
 
 template <Weight T>
-using function_t = std::function<std::vector<T>( const std::vector<T> & )>;
+using function_t =
+    std::function<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>(
+        layer_t<T> & )>;
+template <Weight T>
+using cost_function_t = std::function<layer_t<T>( layer_t<T> &, layer_t<T> & )>;
 
 } // namespace neural
